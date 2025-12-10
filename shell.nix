e19e67020fb31b369ce38bda0f71451966b95746
Mick Sayson <mick@sayson.com>
2025-12-10 12:22:27 -0800

    :)

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  pkgs = import <nixpkgs> { };
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f73d4f0ad010966973bc81f51705cef63683c2f2.tar.gz") {};
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    unstable.zls
+    unstable.zig
+    valgrind
+    gdb
+    python3
+    glfw
+    libGL
+    clang-tools
+    wayland
+    linuxPackages_latest.perf
+    kcov
+    linuxHeaders
+  ];
+
+  LD_LIBRARY_PATH = "${pkgs.wayland}/lib";
+}
